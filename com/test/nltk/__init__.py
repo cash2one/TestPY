cf5d639b03c29bba6f29b5e5555023fb9432ecfb
thunderboltlei <lm82_007@163.com>
2016-11-20 23:43:41 +0800

    nltk：增加 nltk_data 安装使用说明

---
+++ com/test/nltk/__init__.py
@@ -0,0 +1,15 @@
+'''
+说明：
+
+1. 下载 nltk 的数据
+# ipython
+: import nltk
+: nltk.download() 选择安装路径 /opt/development/python/nltk/data
+
+2. 建立软链接到 python 的执行目录，前提是已经重新安装高版本的 python，目前使用 2.7.10
+# cd /usr/local/lib
+# ln -s /opt/development/python/nltk/data ./nltk_data
+
+3. 测试
+执行 TestN03.py，可测试安装结果。
+'''
